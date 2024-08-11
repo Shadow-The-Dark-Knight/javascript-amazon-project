26db93c14fd7fc7961ec6e978cdfc5d3dc716c7f
Shadow <nintendothing123@gmail.com>
2024-08-11 00:23:52 -0700

    Make Payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,12 +4,8 @@ import { formatCurrency} from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js'
+import {renderPaymentSummary} from './paymentSummary.js';
 
-hello();
-
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
 
 // add first parameter is amount and second is units
 // if out of another folder use ../
@@ -154,6 +150,8 @@ export function renderOrderSummary () {
 
        const container = document.querySelector(`.js-cart-item-container-${productId}`)
        container.remove()
+
+       renderPaymentSummary();
      });
      // updateCartQuantity();
    });
@@ -201,6 +199,7 @@ export function renderOrderSummary () {
        const {productId, deliveryOptionId} = element.dataset;
        updateDeliveryOption(productId, deliveryOptionId);
        renderOrderSummary();
+       renderPaymentSummary();
      });
    });
  }
--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -65,3 +65,6 @@ export function renderPaymentSummary() {
   document.querySelector('.js-payment-summary')
   .innerHTML = paymentSummaryHTML;
 }
+// issue that arises is that when I change the delivery option
+// the price doesn't change until I refresh the page
+// solution use renderPaymentSummary so that refreshes automatically
