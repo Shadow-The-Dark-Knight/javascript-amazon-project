fef7df704c3e3f4e21f62212262e9eb903f7479f
Shadow <nintendothing123@gmail.com>
2024-09-07 20:58:04 -0700

    Create oop verision of cart

---
+++ data/notes.js
@@ -0,0 +1,13 @@
+// id is useful bc you (normailize the data)
+//JSON.stringify convert things to strings
+// as localStorage can only be used when it has a string
+
+//JSON.parse is used when using getItem while JSON.stringify
+// is used for setItem
+// if have problems and code looks good use localStorage.clear() 
+//then refresh page.
+
+// function insdie a object = method
+
+////// object orientation programming = oragnize code into objects /////
+// for things that generate objects use PascalCase
